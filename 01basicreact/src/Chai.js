4959444a3faa4cbb2e26ce0db937331afd4b3389
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-02-15 15:18:16 +0530

    Comparing flow to React and vite

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h1>HeLLO CHAI</h1>;
+}
+
+export default Chai;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Chai from "./Chai.jsx/"
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Do you want some?</h1>
+      <p>It really addictive.</p>
+    </>
+  )
+}
+
+export default App
