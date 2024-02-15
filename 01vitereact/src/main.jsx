1c0ed34f3c50c393dc06a4bb1fec953dfa483c57
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-02-15 18:54:21 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+function MyApp() {
+  return (
+    <>
+      <h1>Custom MyApp function</h1>
+    </>
+  );
+}
+
+const someVariable = "I'm used for example";
+
+const anotherElement = (
+  <a href="https://google.com/" target="_blank">
+    Vist Google
+  </a>
+);
+
+const customElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to vist Google",
+  someVariable
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <App /> //Case 1
+  // <MyApp /> //Case 2
+  // anotherElement //Case 3
+  customElement
+);
